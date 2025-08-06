dc1176f2aa47deac92cbb293d33c103249695940
David <lidavid206@gmail.com>
2025-08-06 00:27:00 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection,_ = server_socket.accept() # wait for client
-    connection.sendall(b"PONG\r\n")
+    connection.sendall(b"P0NG\r\n")
 
 
 if __name__ == "__main__":
