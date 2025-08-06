b353fd687193a1a50b8c8e082fa104ed44f7c652
David <lidavid206@gmail.com>
2025-08-06 01:23:32 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,18 +3,18 @@ import threading
 
 ##Takes in multiple clients and handles them concurrently
 def handle_client(client: socket.socket):
-    while chunk := client.recv(4096):
-        if chunk == b"":
-            break
+    while True:
+        input = client.recv(1024)
+        if "ping" in input.decode().strip().lower():
             # Respond with PONG
-        client.sendall(b"+PONG\r\n")
+            client.sendall(b"+PONG\r\n")
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    
-    connection,_ = server_socket.accept() # wait for client
-    threading.Thread(target=handle_client, args=(connection,)).start()
+    while True:
+        connection,_ = server_socket.accept() # wait for client
+        threading.Thread(target=handle_client, args=(connection,)).start()
 
 if __name__ == "__main__":
     main()
