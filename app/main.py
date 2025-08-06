2e5f72d0c06256d5757016ac78f70cf77b45fa06
David <lidavid206@gmail.com>
2025-08-06 01:18:03 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,23 +1,19 @@
 import socket  # noqa: F401
+import threading
+
+##Takes in multiple clients and handles them concurrently
+def handle_client(client: socket.socket):
+    input = client.recv(1024)
+    if "ping" in input.decode().strip().lower():
+        # Respond with PONG
+        client.sendall(b"+PONG\r\n")
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     
     connection,_ = server_socket.accept() # wait for client
-
-    while True:
-        input = connection.recv(1024)
-        if "ping" in input.decode().strip().lower():
-            # Respond with PONG
-            connection.send("+PONG\r\n".encode())
-    
-
+    threading.Thread(target=handle_client, args=(connection,)).start()
 
 if __name__ == "__main__":
     main()
