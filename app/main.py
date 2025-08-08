6a2a2bedb6efd4dc74b9c935380e7ac5c8e566b8
David <lidavid206@gmail.com>
2025-08-08 14:29:34 -0400

    s

--- app/main.py
+++ app/main.py
@@ -89,11 +89,13 @@ def handle_client(client: socket.socket):
             lists[elements[1]] = list
             size = len(lists[elements[1]])
             
-            if elements[1] in blocked_clients:
+            if elements[1] in blocked_clients and len(blocked_clients[elements[1]]) > 0:
                 blocked_client = blocked_clients[elements[1]].pop(0)
                 item = lists[elements[1]].pop(0)
                 message = f"*2\r\n${len(elements[1])}\r\n{elements[1]}\r\n${len(item)}\r\n{item}\r\n"
                 blocked_client.sendall(message.encode())
+            if len(blocked_clients[elements[1]]) == 0:
+                del blocked_clients[elements[1]]
 
             client.sendall(f":{size}\r\n".encode())
         elif "lrange" in elements[0].lower():
@@ -164,7 +166,7 @@ def handle_client(client: socket.socket):
             else:
                 if list_name not in blocked_clients:
                     blocked_clients[list_name] = []
-                    blocked_clients[list_name].append(client)
+                blocked_clients[list_name].append(client)
 
 
 
