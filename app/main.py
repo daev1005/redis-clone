e153b89e27a2d699e2a7dc0e367c543b2d921b6d
David <lidavid206@gmail.com>
2025-08-09 00:50:56 -0400

    s

--- app/main.py
+++ app/main.py
@@ -214,7 +214,9 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         connection,_ = server_socket.accept() # wait for client
-        threading.Thread(target=handle_client, args=(connection,)).start()
+        thread = threading.Thread(target=handle_client, args=(connection,))
+        thread.daemon = True
+        thread.start()
 
 if __name__ == "__main__":
     main()
