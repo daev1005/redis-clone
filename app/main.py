213566f24cfa80b17555613ee5903c7756d101bb
David <lidavid206@gmail.com>
2025-08-23 23:19:20 -0400

    s

--- app/main.py
+++ app/main.py
@@ -448,7 +448,7 @@ def subscribe_cmd(client: socket.socket, elements: list):
         return f"-ERR channel already subscribed"
     else:
         subscribed.append(channel)
-        return make_resp["subscribe", channel, str(len(subscribed))]
+        return make_resp("subscribe", channel, str(len(subscribed)))
 
         
 
